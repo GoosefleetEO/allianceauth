7aa1a2f3368c096cc1d093ed53a5d5e3983fba8f
colcrunch <rhartnett35@gmail.com>
2018-10-07 22:08:03 +0000

    Update SRP module for zKill API changes.

--- allianceauth/srp/managers.py
+++ allianceauth/srp/managers.py
@@ -1,9 +1,15 @@
 from allianceauth import NAME
-
+from esi.clients import esi_client_factory
 import requests
 import logging
+import os
 
 logger = logging.getLogger(__name__)
+SWAGGER_SPEC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'swagger.json')
+"""
+Swagger Operations:
+get_killmails_killmail_id_killmail_hash
+"""
 
 
 class SRPManager:
@@ -26,12 +32,20 @@ class SRPManager:
         r = requests.get(url, headers=headers)
         result = r.json()[0]
         if result:
-            ship_type = result['victim']['ship_type_id']
+            killmail_id = result['killmail_id']
+            killmail_hash = result['zkb']['hash']
+            c = esi_client_factory(spec_file=SWAGGER_SPEC_PATH)
+            km = c.Killmails.get_killmails_killmail_id_killmail_hash(killmail_id=killmail_id,
+                                                                     killmail_hash=killmail_hash).result()
+        else:
+            raise ValueError("Invalid Kill ID")
+        if km:
+            ship_type = km['victim']['ship_type_id']
             logger.debug("Ship type for kill ID %s is %s" % (kill_id, ship_type))
             ship_value = result['zkb']['totalValue']
             logger.debug("Total loss value for kill id %s is %s" % (kill_id, ship_value))
-            victim_id = result['victim']['character_id']
+            victim_id = km['victim']['character_id']
             return ship_type, ship_value, victim_id
         else:
-            raise ValueError("Invalid Kill ID")
+            raise ValueError("Invalid Kill ID or Hash.")
 
