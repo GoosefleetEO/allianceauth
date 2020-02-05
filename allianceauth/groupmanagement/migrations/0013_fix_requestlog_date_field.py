63d2021a73c08de5019a4691b317888ff56bfa0d
Col Crunch <rhartnett35@gmail.com>
2020-02-04 23:23:04 -0500

    Fix migration conflicts.

--- allianceauth/groupmanagement/migrations/0013_fix_requestlog_date_field.py
+++ allianceauth/groupmanagement/migrations/0013_fix_requestlog_date_field.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('groupmanagement', '0011_requestlog_date'),
+        ('groupmanagement', '0012_group_leads'),
     ]
 
     operations = [
