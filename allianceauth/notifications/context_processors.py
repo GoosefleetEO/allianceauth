1ce0dbde0ee7ed203f994664fdb5926ca8c8d293
Aaron Kable <aaronkable@gmail.com>
2020-03-16 02:09:24 +0000

    stop the notification context profider from hitting the database for each menu hook

--- allianceauth/notifications/context_processors.py
+++ allianceauth/notifications/context_processors.py
@@ -1,5 +1,10 @@
 from .models import Notification
-
+from django.core.cache import cache
 
 def user_notification_count(request):
-    return {'notifications': len(Notification.objects.filter(user__id=request.user.id).filter(viewed=False))}
+    notification_count = cache.get("u-note:{}".format(request.user.id), -1)
+    if notification_count<0:
+        notification_count = Notification.objects.filter(user__id=request.user.id).filter(viewed=False).count()
+        cache.set("u-note:{}".format(request.user.id),notification_count,5)
+
+    return {'notifications': notification_count}
