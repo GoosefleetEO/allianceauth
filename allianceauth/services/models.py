f63434adc339037f97af92a3bcfbc43e363382c0
ErikKalkoken <erik.kalkoken@gmail.com>
2020-03-21 14:41:45 +0100

    Fix broken link and remove outdated migrations for services name formatter

--- allianceauth/services/models.py
+++ allianceauth/services/models.py
@@ -4,14 +4,30 @@ from allianceauth.authentication.models import State
 
 
 class NameFormatConfig(models.Model):
-    service_name = models.CharField(max_length=100, blank=False, null=False)
-    default_to_username = models.BooleanField(default=True, help_text="If a user has no main_character, "
-                                                                      "default to using their Auth username instead.")
-    format = models.CharField(max_length=100, blank=False, null=False,
-                              help_text='For information on constructing name formats, please see the '
-                                        '<a href="https://allianceauth.readthedocs.io/en/latest/features/nameformats">'
-                                        'name format documentation</a>')
-    states = models.ManyToManyField(State, help_text="States to apply this format to. You should only have one "
-                                                     "formatter for each state for each service.")
+    service_name = models.CharField(max_length=100, blank=False)
+    default_to_username = models.BooleanField(
+        default=True, 
+        help_text=
+            'If a user has no main_character, '
+            'default to using their Auth username instead.'
+    )
+    format = models.CharField(
+        max_length=100,
+        blank=False,
+        help_text=
+            'For information on constructing name formats '
+            'please see the official documentation, '
+            'topic "Services Name Formats".'
+    )
+    states = models.ManyToManyField(
+        State, 
+        help_text=
+            "States to apply this format to. You should only have one "
+            "formatter for each state for each service."        
+    )
 
+    def __str__(self):
+        return '%s: %s' % (
+            self.service_name, ', '.join([str(x) for x in self.states.all()])
+        )
 
