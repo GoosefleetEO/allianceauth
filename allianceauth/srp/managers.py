b6fdf840efb73508810a25e22c6cc0b6a38fbae0
ErikKalkoken <erik.kalkoken@gmail.com>
2020-03-25 18:00:23 +0100

    Update swagger files and remove swagger fle dependency from srp package

--- allianceauth/srp/managers.py
+++ allianceauth/srp/managers.py
@@ -1,21 +1,17 @@
-from allianceauth import NAME
-from esi.clients import esi_client_factory
-import requests
 import logging
 import os
 
+import requests
+
+from allianceauth import NAME
+from allianceauth.eveonline.providers import provider
+
+
 logger = logging.getLogger(__name__)
-SWAGGER_SPEC_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'swagger.json')
-"""
-Swagger Operations:
-get_killmails_killmail_id_killmail_hash
-"""
 
 
 class SRPManager:
-    def __init__(self):
-        pass
-
+    
     @staticmethod
     def get_kill_id(killboard_link):
         num_set = '0123456789'
@@ -34,18 +30,23 @@ class SRPManager:
         if result:
             killmail_id = result['killmail_id']
             killmail_hash = result['zkb']['hash']
-            c = esi_client_factory(spec_file=SWAGGER_SPEC_PATH)
-            km = c.Killmails.get_killmails_killmail_id_killmail_hash(killmail_id=killmail_id,
-                                                                     killmail_hash=killmail_hash).result()
+            c = provider.client
+            km = c.Killmails.get_killmails_killmail_id_killmail_hash(
+                killmail_id=killmail_id,
+                killmail_hash=killmail_hash
+            ).result()
         else:
             raise ValueError("Invalid Kill ID")
         if km:
             ship_type = km['victim']['ship_type_id']
-            logger.debug("Ship type for kill ID %s is %s" % (kill_id, ship_type))
+            logger.debug(
+                "Ship type for kill ID %s is %s" % (kill_id, ship_type)
+            )
             ship_value = result['zkb']['totalValue']
-            logger.debug("Total loss value for kill id %s is %s" % (kill_id, ship_value))
+            logger.debug(
+                "Total loss value for kill id %s is %s" % (kill_id, ship_value)
+            )
             victim_id = km['victim']['character_id']
             return ship_type, ship_value, victim_id
         else:
             raise ValueError("Invalid Kill ID or Hash.")
-
