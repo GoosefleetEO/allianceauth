d7dcacb899bf7bc13cfd4176748282a2e9e3ac1c
Aaron Kable <aaronkable@gmail.com>
2020-04-17 06:45:01 +0000

    Add 500 and 400, 403, 404 error redirects back to dashboard with basic message

--- allianceauth/project_template/project_name/urls.py
+++ allianceauth/project_template/project_name/urls.py
@@ -4,3 +4,8 @@ from allianceauth import urls
 urlpatterns = [
     url(r'', include(urls)),
 ]
+
+handler500 = 'allianceauth.views.Generic500Redirect'
+handler404 = 'allianceauth.views.Generic404Redirect'
+handler403 = 'allianceauth.views.Generic403Redirect'
+handler400 = 'allianceauth.views.Generic400Redirect'
