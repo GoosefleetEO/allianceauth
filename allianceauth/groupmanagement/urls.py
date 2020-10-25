281dbdbb01eba4a8e57a9f7d51278dac9f493e80
Peter Pfeufer <info@ppfeufer.de>
2020-10-25 11:21:35 +0100

    proper URL structure established

--- allianceauth/groupmanagement/urls.py
+++ allianceauth/groupmanagement/urls.py
@@ -1,29 +1,52 @@
 from . import views
 
 from django.conf.urls import url
-app_name = 'groupmanagement'
+
+app_name = "groupmanagement"
 
 urlpatterns = [
-    url(r'^groups/', views.groups_view, name='groups'),    
-    url(r'^management/', views.group_management,
-        name='management'),
-    url(r'^membership/$', views.group_membership,
-        name='membership'),
-    url(r'^membership/(\w+)/$', views.group_membership_list,
-        name='membership_list'),
-    url(r'^membership/(\w+)/audit/$', views.group_membership_audit, name="audit_log"),
-    url(r'^membership/(\w+)/remove/(\w+)/$', views.group_membership_remove,
-        name='membership_remove'),
-    url(r'^request_add/(\w+)', views.group_request_add,
-        name='request_add'),
-    url(r'^request/accept/(\w+)', views.group_accept_request,
-        name='accept_request'),
-    url(r'^request/reject/(\w+)', views.group_reject_request,
-        name='reject_request'),
-    url(r'^request_leave/(\w+)', views.group_request_leave,
-        name='request_leave'),
-    url(r'leave_request/accept/(\w+)', views.group_leave_accept_request,
-        name='leave_accept_request'),
-    url(r'^leave_request/reject/(\w+)', views.group_leave_reject_request,
-        name='leave_reject_request'),
+    # groups
+    url(r"^groups/$", views.groups_view, name="groups"),
+    url(r"^group/request/join/(\w+)/$", views.group_request_add, name="request_add"),
+    url(
+        r"^group/request/leave/(\w+)/$", views.group_request_leave, name="request_leave"
+    ),
+    # group management
+    url(r"^groupmanagement/requests/$", views.group_management, name="management"),
+    url(r"^groupmanagement/membership/$", views.group_membership, name="membership"),
+    url(
+        r"^groupmanagement/membership/(\w+)/$",
+        views.group_membership_list,
+        name="membership",
+    ),
+    url(
+        r"^groupmanagement/membership/(\w+)/audit-log/$",
+        views.group_membership_audit,
+        name="audit_log",
+    ),
+    url(
+        r"^groupmanagement/membership/(\w+)/remove/(\w+)/$",
+        views.group_membership_remove,
+        name="membership_remove",
+    ),
+    url(
+        r"^groupmanagement/request/join/accept/(\w+)/$",
+        views.group_accept_request,
+        name="accept_request",
+    ),
+    url(
+        r"^groupmanagement/request/join/reject/(\w+)/$",
+        views.group_reject_request,
+        name="reject_request",
+    ),
+    url(
+        r"groupmanagement/request/leave/accept/(\w+)/$",
+        views.group_leave_accept_request,
+        name="leave_accept_request",
+    ),
+    url(
+        r"^groupmanagement/request/leave/reject/(\w+)/$",
+        views.group_leave_reject_request,
+        name="leave_reject_request",
+    ),
 ]
