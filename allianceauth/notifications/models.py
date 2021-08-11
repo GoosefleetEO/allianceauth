1e133b7c5d814b197522372fc09f848df6e2a07e
Erik Kalkoken <erik.kalkoken@gmail.com>
2021-08-11 06:06:23 +0000

    Improve notifications

--- allianceauth/notifications/models.py
+++ allianceauth/notifications/models.py
@@ -2,6 +2,7 @@ import logging
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 from .managers import NotificationManager
 
@@ -13,17 +14,43 @@ class Notification(models.Model):
     
     NOTIFICATIONS_MAX_PER_USER_DEFAULT = 50
     NOTIFICATIONS_REFRESH_TIME_DEFAULT = 30
-    
-    LEVEL_CHOICES = (
-        ('danger', 'CRITICAL'),
-        ('danger', 'ERROR'),
-        ('warning', 'WARN'),
-        ('info', 'INFO'),
-        ('success', 'DEBUG'),
-    )
+
+    class Level(models.TextChoices):
+        """A notification level."""
+
+        DANGER = 'danger', _('danger')  #: 
+        WARNING = 'warning', _('warning')  #:
+        INFO = 'info', _('info')  #:
+        SUCCESS = 'success', _('success')  #:
+
+        @classmethod
+        def from_old_name(cls, name: str) -> object:
+            """Map old name to enum.
+            
+            Raises ValueError for invalid names.
+            """            
+            name_map = {
+                "CRITICAL": cls.DANGER,
+                "ERROR": cls.DANGER,
+                "WARN": cls.WARNING,
+                "INFO": cls.INFO,
+                "DEBUG": cls.SUCCESS,
+            }
+            try:
+                return name_map[name]
+            except KeyError:
+                raise ValueError(f"Unknown name: {name}") from None
+
+    # LEVEL_CHOICES = (
+    #     ('danger', 'CRITICAL'),
+    #     ('danger', 'ERROR'),
+    #     ('warning', 'WARN'),
+    #     ('info', 'INFO'),
+    #     ('success', 'DEBUG'),
+    # )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    level = models.CharField(choices=LEVEL_CHOICES, max_length=10)
+    level = models.CharField(choices=Level.choices, max_length=10, default=Level.INFO)
     title = models.CharField(max_length=254)
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True, db_index=True)
@@ -45,22 +72,15 @@ class Notification(models.Model):
         Notification.objects.invalidate_user_notification_cache(self.user.pk)
 
     def mark_viewed(self) -> None:
-        """mark notification as viewed"""
+        """Mark notification as viewed."""
         logger.info("Marking notification as viewed: %s" % self)
         self.viewed = True
         self.save()
 
     def set_level(self, level_name: str) -> None:
-        """set notification level according to level name, e.g. 'CRITICAL'
+        """Set notification level according to old level name, e.g. 'CRITICAL'.
         
-        raised exception on invalid level names
-        """
-        try:
-            new_level = [
-                item[0] for item in self.LEVEL_CHOICES if item[1] == level_name
-            ][0]
-        except IndexError:
-            raise ValueError('Invalid level name: %s' % level_name)
-
-        self.level = new_level
+        Raises ValueError on invalid level names.
+        """        
+        self.level = self.Level.from_old_name(level_name)
         self.save()
