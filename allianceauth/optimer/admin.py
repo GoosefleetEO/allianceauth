2c59cc4cc3dd1c3ae5a0ca3b0e9f8f9d89d2da8d
Peter Pfeufer <info@ppfeufer.de>
2021-10-30 04:34:49 +0000

    Add types and description to optimers

--- allianceauth/optimer/admin.py
+++ allianceauth/optimer/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from allianceauth.optimer.models import OpTimer
+from allianceauth.optimer.models import OpTimer, OpTimerType
 
+admin.site.register(OpTimerType)
 admin.site.register(OpTimer)
