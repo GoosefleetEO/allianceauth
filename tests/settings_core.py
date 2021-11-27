60b12bad61e1683fbaf763180bcdcb31e3cc3d54
Ariel Rin <ozirascal@gmail.com>
2021-11-27 03:35:40 +0000

    Python 3.10 Support

--- tests/settings_core.py
+++ tests/settings_core.py
@@ -6,21 +6,9 @@ Testing core packages only
 
 from allianceauth.project_template.project_name.settings.base import *
 
-# Use nose to run all tests
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-NOSE_ARGS = [
-    #'--with-coverage',
-    #'--cover-package=',
-    #'--exe',  # If your tests need this to be found/run, check they py files are not chmodded +x
-]
-
 # Celery configuration
 CELERY_ALWAYS_EAGER = True  # Forces celery to run locally for testing
 
-INSTALLED_APPS += [
-    'django_nose',
-]
 
 ROOT_URLCONF = 'tests.urls'
 
