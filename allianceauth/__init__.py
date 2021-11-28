7767c46bf4f8378a5af7bbf69e5dfb65b3c190f0
Ariel Rin <ozirascal@gmail.com>
2021-11-28 15:37:17 +0000

    Version Bump v2.9.3

--- allianceauth/__init__.py
+++ allianceauth/__init__.py
@@ -1,7 +1,7 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 
-__version__ = '2.9.2'
+__version__ = '2.9.3'
 __title__ = 'Alliance Auth'
 __url__ = 'https://gitlab.com/allianceauth/allianceauth'
 NAME = f'{__title__} v{__version__}'
