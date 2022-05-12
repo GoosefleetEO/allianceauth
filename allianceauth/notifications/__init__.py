75bccf1b0fe90889537f2a6a4ef7e2f397d4374a
Erik Kalkoken <erik.kalkoken@gmail.com>
2022-05-12 04:02:17 +0000

    Improve notifications

--- allianceauth/notifications/__init__.py
+++ allianceauth/notifications/__init__.py
@@ -1,9 +1,3 @@
+from .core import notify  # noqa: F401
+
 default_app_config = 'allianceauth.notifications.apps.NotificationsConfig'
-
-
-def notify(
-    user: object, title: str, message: str = None, level: str = 'info'
-) -> None:
-    """Sends a new notification to user. Convenience function to manager pendant."""
-    from .models import Notification
-    Notification.objects.notify_user(user, title, message, level)
--- allianceauth/notifications/tests/test_init.py
+++ allianceauth/notifications/tests/test_init.py
@@ -4,11 +4,8 @@ from allianceauth.tests.auth_utils import AuthUtils
 from .. import notify
 from ..models import Notification
 
-MODULE_PATH = 'allianceauth.notifications'
-
 
 class TestUserNotificationCount(TestCase):
-
     @classmethod
     def setUpTestData(cls):
         cls.user = AuthUtils.create_user('magic_mike')
@@ -23,6 +20,18 @@ class TestUserNotificationCount(TestCase):
             alliance_name='RIDERS'
         )
 
-    def test_can_notify(self):
-        notify(self.user, 'dummy')
+    def test_can_notify_short(self):
+        # when
+        notify(self.user, "dummy")
+        # then
         self.assertEqual(Notification.objects.filter(user=self.user).count(), 1)
+
+    def test_can_notify_full(self):
+        # when
+        notify(user=self.user, title="title", message="message", level="danger")
+        # then
+        obj = Notification.objects.first()
+        self.assertEqual(obj.user, self.user)
+        self.assertEqual(obj.title, "title")
+        self.assertEqual(obj.message, "message")
+        self.assertEqual(obj.level, "danger")
