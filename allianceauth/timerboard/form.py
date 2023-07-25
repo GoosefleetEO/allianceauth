951c4135c219012bd05e4d04ff854dd8559f582f
Hamish W <hamfish12@gmail.com>
2023-07-25 09:26:10 +0000

    Adding Absolute Timers to base timerboard

--- allianceauth/timerboard/form.py
+++ allianceauth/timerboard/form.py
@@ -61,13 +61,16 @@ class TimerForm(forms.ModelForm):
     structure = forms.ChoiceField(choices=structure_choices, required=True, label=_("Structure Type"))
     timer_type = forms.ChoiceField(choices=TimerType.choices, label=_("Timer Type"))
     objective = forms.ChoiceField(choices=objective_choices, required=True, label=_("Objective"))
-    days_left = forms.IntegerField(required=True, label=_("Days Remaining"), validators=[MinValueValidator(0)])
-    hours_left = forms.IntegerField(required=True, label=_("Hours Remaining"),
+    absolute_checkbox = forms.BooleanField(label=_("Absolute Timer"), required=False, initial=False)
+    absolute_time = forms.CharField(required=False,label=_("Date and Time"))
+    days_left = forms.IntegerField(required=False, label=_("Days Remaining"), validators=[MinValueValidator(0)])
+    hours_left = forms.IntegerField(required=False, label=_("Hours Remaining"),
                                     validators=[MinValueValidator(0), MaxValueValidator(23)])
-    minutes_left = forms.IntegerField(required=True, label=_("Minutes Remaining"),
+    minutes_left = forms.IntegerField(required=False, label=_("Minutes Remaining"),
                                         validators=[MinValueValidator(0), MaxValueValidator(59)])
     important = forms.BooleanField(label=_("Important"), required=False)
     corp_timer = forms.BooleanField(label=_("Corp-Restricted"), required=False)
+
 
     def save(self, commit=True):
         timer = super().save(commit=False)
@@ -77,18 +80,30 @@ class TimerForm(forms.ModelForm):
         corporation = character.corporation
         logger.debug("Determined timer save request on behalf "
                     "of character {} corporation {}".format(character, corporation))
-        # calculate future time
-        future_time = datetime.timedelta(days=self.cleaned_data['days_left'], hours=self.cleaned_data['hours_left'],
-                                        minutes=self.cleaned_data['minutes_left'])
-        current_time = timezone.now()
-        eve_time = current_time + future_time
-        logger.debug(
-            f"Determined timer eve time is {eve_time} - current time {current_time}, adding {future_time}")
+
+        days_left = self.cleaned_data['days_left']
+        hours_left = self.cleaned_data['hours_left']
+        minutes_left = self.cleaned_data['minutes_left']
+        absolute_time = self.cleaned_data['absolute_time']
+
+        if days_left or hours_left or minutes_left:
+            # Calculate future time
+            future_time = datetime.timedelta(days=days_left, hours=hours_left, minutes=minutes_left)
+            current_time = timezone.now()
+            eve_time = current_time + future_time
+            logger.debug(f"Determined timer eve time is {eve_time} - current time {current_time}, adding {future_time}")
+        elif absolute_time:
+            # Use absolute time
+            eve_time = absolute_time
+        else:
+            raise ValueError("Either future time or absolute time must be provided.")
 
         timer.eve_time = eve_time
         timer.eve_character = character
         timer.eve_corp = corporation
         timer.user = self.user
+
         if commit:
             timer.save()
+
         return timer
